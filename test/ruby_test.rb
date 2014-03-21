d883a10928162aa79309366cce62fbe5ca9a6164
gnuhub <gnuhub@gmail.com>
2014-03-22 00:50:48 +0800

    ruby install

--- test/ruby_test.rb
+++ test/ruby_test.rb
@@ -8,4 +8,23 @@ class SshCopyIdTest < MiniTest::Unit::TestCase
 		b = s.include? "ibm_db"
 		assert(b,"ibm_db gem is not installed")
 	end
+	def test_ruby_install
+		puts ""
+		puts "test ruby install"
+		s = %x(gem list)
+		puts s
+		b1 = s.include? "bundler"
+		assert(b1,"bundler gem is not installed")
+		b2 = s.include? "pry"
+		assert(b2,"pry gem is not installed")
+		b3 = s.include? "byebug"
+		assert(b3,"byebug gem is not installed")
+		b4 = s.include? "json"
+		assert(b4,"json gem is not installed")
+		b5 = s.include? "watir-webdriver"
+		assert(b5,"watir-webdriver gem is not installed")
+		b6 = s.include? "rails"
+		assert(b6,"rails gem is not installed")
+		
+	end
 end
